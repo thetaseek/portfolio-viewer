087d4cf7e59f4c36bcae1656d82a78b949330524
thetaseek <snoozesatoshi@protonmail.com>
2020-04-28 12:25:27 +0800

    Updates

---
+++ precache-manifest.f5a704ec7c84fb614403bf5a01204101.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720d75936f1fb97b0eda568fc620566c",
+    "url": "/portfolio-viewer/index.html"
+  },
+  {
+    "revision": "7d9da878795eb566ce5e",
+    "url": "/portfolio-viewer/static/js/2.7187b259.chunk.js"
+  },
+  {
+    "revision": "64852569161b7cf61c9a91245eefd384",
+    "url": "/portfolio-viewer/static/js/2.7187b259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f6afce21927cdd7212",
+    "url": "/portfolio-viewer/static/js/main.3fe95468.chunk.js"
+  },
+  {
+    "revision": "6b46d1aeda91bb45f609",
+    "url": "/portfolio-viewer/static/js/runtime-main.499085fa.js"
+  }
+]);
